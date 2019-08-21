80cd2c1ec0b9afe7289ef6f0920542335b2d1452
Didier 'OdyX' Raboud <didier.raboud@liip.ch>
2019-08-21 18:36:29 +0200

    Initial working clone, with variables setup

--- japi.py
+++ japi.py
@@ -1,4 +1,5 @@
 import click
+import json
 import logging
 import os
 import requests
@@ -30,7 +31,9 @@ class JelasticAPI:
         # Make sure we have our session in
         self.logger.debug("_apicall {} {}, data:{}".format(method.upper(), uri, data))
         data.update(self.apidata)
-        r = getattr(requests, method)(self.apiurl + uri, data)
+        r = getattr(requests, method)(
+            "{url}{uri}".format(url=self.apiurl, uri=uri), data
+        )
         if r.status_code != requests.codes.ok:
             raise JelasticAPIException(
                 "{method} to {uri} failed with HTTP code {code}".format(
@@ -91,5 +94,67 @@ class JelasticAPI:
         environment.Control.CloneEnv Jelastic API call
         """
         return self.post(
-            "/environment/control/rest/cloneenv", {"srcEnvName": srcenvName}
+            "/environment/control/rest/cloneenv",
+            {"srcEnvName": srcEnvName, "dstEnvName": dstEnvName},
         )
+
+    def AddContainerEnvVars(
+        self, envName: str, nodeGroup: str = "", nodeId: str = "", vars: Dict = {}
+    ) -> Dict:
+        """
+        environment.Control.AddContainerEnvVars Jelastic API call
+        """
+        params = {"envName": envName, "vars": json.dumps(vars)}
+        if nodeGroup:
+            params["nodeGroup"] = nodeGroup
+        elif nodeId:
+            params["nodeId"] = nodeId
+        return self.post("/environment/control/rest/addcontainerenvvars", params)
+
+    def AttachEnvGroup(
+        self, envName: str, envGroup: str = "", envGroups: str = ""
+    ) -> Dict:
+        """
+        environment.Control.AttachEnvGroup Jelastic API call
+        """
+        params = {"envName": envName}
+        if envGroup:
+            params["envGroup"] = envGroup
+        elif envGroups:
+            params["envGroups"] = envGroups
+        return self.post("/environment/control/rest/attachenvgroup", params)
+
+    def DetachEnvGroup(
+        self, envName: str, envGroup: str = "", envGroups: str = ""
+    ) -> Dict:
+        """
+        environment.Control.DetachEnvGroup Jelastic API call
+        """
+        params = {"envName": envName}
+        if envGroup:
+            params["envGroup"] = envGroup
+        elif envGroups:
+            params["envGroups"] = envGroups
+        return self.post("/environment/control/rest/detachenvgroup", params)
+
+    def RestartNodes(
+        self,
+        envName: str,
+        nodeGroup: str = "",
+        nodeId: str = "",
+        delay: int = 0,
+        isSequential: bool = None,
+    ) -> Dict:
+        """
+        environment.Control.RestartNodes Jelastic API call
+        """
+        params = {"envName": envName}
+        if nodeGroup:
+            params["nodeGroup"] = nodeGroup
+        elif nodeId:
+            params["nodeId"] = nodeId
+        if delay:
+            params["delay"] = delay
+        if isSequential is not None:
+            params["isSequential"] = isSequential
+        return self.post("/environment/control/rest/restartnodes", params)

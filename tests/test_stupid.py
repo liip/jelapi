3f6d0abceb2082ccfdd3f3a15122961a726050ea
Didier 'OdyX' Raboud <didier.raboud@liip.ch>
2021-01-28 16:22:03 +0100

    Flake8 all'em

--- tests/test_stupid.py
+++ tests/test_stupid.py
@@ -1,7 +1,6 @@
-import pytest
-
 from jelapi import JelasticAPI
 
+
 def test_load_class():
-    japic = JelasticAPI(apiurl='https://api.example.com', apitoken='string')
+    JelasticAPI(apiurl="https://api.example.com", apitoken="string")
     assert True

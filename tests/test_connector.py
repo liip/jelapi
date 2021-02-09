dc5cb7a4b70e1f854195b10a111b1821d0b997d6
Didier 'OdyX' Raboud <didier.raboud@liip.ch>
2021-02-09 14:51:31 +0100

    Test function formatting

--- tests/test_connector.py
+++ tests/test_connector.py
@@ -35,3 +35,27 @@ def test_connector_failed_empty_answer():
     with pytest.raises(JelasticAPIException):
         japic._("Environment.Control.GetEnvs")
         assert getenvs_route.called
+
+
+def test_connector_refuses_wrongly_formatted_functions():
+    """
+    Check that functions need to be in the First.Second.Word format
+    """
+    japic = JelasticAPIConnector(apiurl=APIURL, apitoken="string")
+
+    with pytest.raises(JelasticAPIException):
+        # 3 dots
+        japic._("Not.A.Function.Call")
+
+    with pytest.raises(JelasticAPIException):
+        # 2 dots
+        japic._("Not_A.Function_Call")
+
+    with respx.mock() as respx_mock:
+        # 3 dots is a valid function
+        # TODO: Perhaps we allow-list the functions we know
+        uncontrolled_route = respx_mock.post(f"{APIURL}a/function/rest/call").mock(
+            return_value=Response(status_code=200, json={"result": 0})
+        )
+        japic._("A.Function.Call")
+        assert uncontrolled_route.called
